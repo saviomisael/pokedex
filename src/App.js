ad99a67e1b48ccee827a080937d5828ce11ab174
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-26 15:59:06 -0300

    refactor App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Pokedex } from './components/Pokedex';
 import { BaseStyles } from './styles/base';
+import { StoreProvider } from './store/StoreProvider';
 
 const App = () => {
   return (
-    <>
+    <StoreProvider>
       <BaseStyles />
       <Pokedex />
-    </>
+    </StoreProvider>
   );
 };
 
