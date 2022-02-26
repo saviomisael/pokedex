f44131d8d8da9c0b4271c69ad3444009cf1066e2
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-26 15:34:57 -0300

    pass test 'should render favorite button when pokemon is favorite'

---
+++ src/components/PokemonItem/PokemonItem.spec.js
@@ -0,0 +1,21 @@
+import { screen } from '@testing-library/react';
+import { renderWithBaseStyles } from '../../utils/styles-utils';
+import { PokemonItem } from '.';
+import { pokemonsMappedMock } from '../../mocks/pokemonsMock';
+
+describe('<PokemonItem />', () => {
+  it('should render favorite button when pokemon is favorite', () => {
+    const mock = { ...pokemonsMappedMock[0], isFavorite: true };
+
+    renderWithBaseStyles(<PokemonItem pokemon={mock} />);
+
+    expect(screen.getByTestId('buttonFavorite')).toHaveStyleRule(
+      'opacity',
+      '1',
+    );
+    expect(screen.getByTestId('buttonFavorite')).toHaveStyleRule(
+      'visibility',
+      'visible',
+    );
+  });
+});
