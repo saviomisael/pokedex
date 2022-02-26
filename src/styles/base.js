b1d97b7ef86f679c30b50cbc2d9c3ccf741c5852
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-26 13:24:47 -0300

    add base styles

---
+++ src/styles/base.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from './normalize';
+
+export const BaseStyles = createGlobalStyle`
+  ${normalize}
+
+  *,
+  *::after,
+  *::before {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: inherit;
+  }
+  html,
+  body {
+    font-size: 62.5%;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
