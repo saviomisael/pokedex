5ce9c198c897372303366f71dd5f37b9a10d6ec6
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-26 15:58:56 -0300

    refactor Pokedex

--- src/components/Pokedex/index.jsx
+++ src/components/Pokedex/index.jsx
@@ -1,3 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { fetchPokemons } from '../../store/ducks/pokemons-duck';
+import { PokemonsList } from '../PokemonsList';
+
 export const Pokedex = () => {
-  return <main></main>;
+  const dispatch = useDispatch();
+  const {
+    pokemons: { pokemonsList },
+  } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(fetchPokemons());
+  }, [dispatch]);
+
+  return (
+    <main>
+      <PokemonsList pokemons={pokemonsList} />
+    </main>
+  );
 };
