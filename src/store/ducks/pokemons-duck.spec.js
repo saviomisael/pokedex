42f061319512d3add5054fb1b278b15633e4467c
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-26 12:02:21 -0300

    pass test 'should toggle isFavorite property of pokemon by id'

--- src/store/ducks/pokemons-duck.spec.js
+++ src/store/ducks/pokemons-duck.spec.js
@@ -14,4 +14,26 @@ describe('pokemons duck', () => {
 
     expect(stateModified.pokemonsList).toHaveLength(4);
   });
+
+  it('should toggle isFavorite property of pokemon by id', () => {
+    const pokemonId = '09c7c9e4-e32b-4f6a-9bfe-5b55c8e69d93';
+
+    let stateModified = pokemonsReducer(
+      { pokemonsList: [...pokemonsMappedMock] },
+      pokemonsActions.toggleIsFavoriteByPokemonId(pokemonId),
+    );
+
+    expect(
+      stateModified.pokemonsList.find((x) => x.id === pokemonId).isFavorite,
+    ).toBeTruthy();
+
+    stateModified = pokemonsReducer(
+      stateModified,
+      pokemonsActions.toggleIsFavoriteByPokemonId(pokemonId),
+    );
+
+    expect(
+      stateModified.pokemonsList.find((x) => x.id === pokemonId).isFavorite,
+    ).toBeFalsy();
+  });
 });
