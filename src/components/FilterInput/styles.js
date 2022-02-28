706a8a0aba1835b206e553400ce8ebde301a0ccd
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-28 11:44:09 -0300

    add FilterInput styles

---
+++ src/components/FilterInput/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  font-size: 1.8rem;
+  font-weight: bold;
+`;
+
+export const Select = styled.select`
+  text-align: center;
+  outline: none;
+  border: none;
+`;
+
+export const Filter = styled.option``;
