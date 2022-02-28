534277816fd45cd9fd7148114ab5bc8a1f918912
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-28 13:06:46 -0300

    add Pokedex styles

---
+++ src/components/Pokedex/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const MainContent = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  max-width: 69rem;
+  margin: auto;
+  row-gap: 2rem;
+`;
