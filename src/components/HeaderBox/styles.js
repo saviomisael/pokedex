ab22d14fbcadf777e146cb0a8fa502356b6ea20b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-02-28 15:53:18 -0300

    add HeaderBox styles

---
+++ src/components/HeaderBox/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1.5rem 0;
+  column-gap: 7.5rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 3.6rem;
+  font-weight: 300;
+  color: #ca0d29;
+`;
+
+export const Image = styled.img`
+  width: 15rem;
+  height: 15rem;
+`;
